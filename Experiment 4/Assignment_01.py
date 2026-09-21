0c496398f5d07b1f432e76c1062e709af9fa7802
Pritam Das <das27102003@gmail.com>
2026-09-21 15:06:27 +0530

    Update Assignment_01.py

--- Experiment 4/Assignment_01.py
+++ Experiment 4/Assignment_01.py
@@ -6,28 +6,21 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# 1. Dataset
-# Area in square feet
 X = np.array([500, 700, 900, 1100, 1300, 1500]).reshape(-1, 1)
 
-# Price in lakh
 y = np.array([20, 28, 35, 43, 50, 58])
 
-# 2. Create and train model
 model = LinearRegression()
 model.fit(X, y)
 
-# 3. Prediction
 y_pred = model.predict(X)
 
-# 4. Predict price for a new house of 1000 sq. ft.
 new_area = np.array([[1000]])
 predicted_price = model.predict(new_area)
 
 print("Predicted price for 1000 sq. ft.:",
       predicted_price[0])
 
-# 5. Evaluation
 mae = mean_absolute_error(y, y_pred)
 mse = mean_squared_error(y, y_pred)
 rmse = np.sqrt(mse)
@@ -39,7 +32,6 @@ print("MSE :", round(mse, 2))
 print("RMSE:", round(rmse, 2))
 print("R2  :", round(r2, 4))
 
-# 6. Plot
 plt.scatter(X, y, color="blue", label="Actual Prices")
 plt.plot(X, y_pred, color="red", label="Regression Line")
 
